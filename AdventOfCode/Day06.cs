38ac50a61c493b17219b7a50f1d1ec18a6102e15
Anders Bo Rasmussen <anras573@gmail.com>
2022-12-06 07:40:22 +0100

    Finish Advent of COde 2022 Day 06.

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day06()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+    
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {Part1()}");
+
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {Part2()}");
+
+    private int Part1()
+    {
+        return Solve(4);
+    }
+
+    private int Part2()
+    {
+        return Solve(14);
+    }
+    
+    private int Solve(int limit)
+    {
+        var endOfMarker = 0;
+        var queue = new Queue<char>();
+        var line = _input[0];
+
+        while (endOfMarker <= line.Length)
+        {
+            if (queue.Contains(line[endOfMarker]))
+            {
+                while (queue.Dequeue() != line[endOfMarker]) { }
+            }
+            
+            queue.Enqueue(line[endOfMarker]);
+            endOfMarker++;
+            
+            if (queue.Count == limit)
+                return endOfMarker;
+        }
+        
+        return endOfMarker;
+    }
+}
